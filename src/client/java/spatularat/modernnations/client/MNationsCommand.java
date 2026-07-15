21e57f42ffb3188db6d952edc46cdd6f8a034c46
Spatula_Rat <spatularatcoding@gmail.com>
2026-07-15 01:22:55 -0700

    Switch from Minecraft Maps to Yarn

---
+++ src/client/java/spatularat/modernnations/client/MNationsCommand.java
@@ -0,0 +1,8 @@
+package spatularat.modernnations.client.command;
+
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.network.chat.Component;
+
+
+
--- src/client/java/spatularat/modernnations/client/ModernNationsClient.java
+++ src/client/java/spatularat/modernnations/client/ModernNationsClient.java
@@ -1,10 +1,17 @@
 package spatularat.modernnations.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import spatularat.modernnations.client.command.MNationsCommand;
 
 public class ModernNationsClient implements ClientModInitializer {
 	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	public void onInitializeClient(){
+
+		CommandRegistrationCallback.EVENT.register(
+				(dispatcher, registryAccess, environment) -> {
+					MNationsCommand.register(dispatcher);
+				}
+		);
 	}
 }
